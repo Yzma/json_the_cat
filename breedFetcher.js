812415ca312a5cac6edf6c88b0b91a4f25d1c78f
Yzma <10090913+Yzma@users.noreply.github.com>
2023-01-17 15:47:49 -0500

    Refactor to use callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,22 @@
 const request = require('request')
 
-const args = process.argv.slice(2)
+//fetchBreedDescription(breedName, (error, desc) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error || (response && response.statusCode !== 200)) {
+      callback(error, null)
+      return
+    }
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  console.log('error:', error) // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-  console.log('body:', body) // Print the HTML for the Google homepage.
+    const data = JSON.parse(body)
+  
+    if (!data || data.length === 0) {
+      callback('Cat not found', null)
+      return
+    }
+  
+    callback(null, data[0].description)
+  })
+}
 
-  if (error || (response && response.statusCode !== 200)) {
-    console.log('Error fetching data')
-    return
-  }
-
-  if (!body) {
-    console.log('Cat not found')
-    return
-  }
-
-  const data = JSON.parse(body)
-
-  if (!data || data.length === 0) {
-    console.log('Cat not found')
-    return
-  }
-
-  console.log(`${data[0].name} - ${data[0].description}`)
-})
-
+module.exports = { fetchBreedDescription }
